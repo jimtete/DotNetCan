5e375f2bb743bc156982a8eebd3babb0e7bf2430
jimtete <tetepoulidis@ieee.org>
2022-10-31 12:04:39 +0200

    Beggining of JSON Course

---
+++ SerializerV2/SerializerV2/Models/Person.cs
@@ -0,0 +1,14 @@
+namespace SerializerV2.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public int Age { get; set; }
+
+    public bool IsAlive { get; set; }
+}
---
+++ SerializerV2/SerializerV2/Program.cs
@@ -0,0 +1,11 @@
+﻿using SerializerV2.Models;
+
+var person = new Person
+{
+    Id = 1,
+    FirstName = "Sean",
+    LastName = "Connery",
+    Age = 90,
+    IsAlive = false
+};
+
