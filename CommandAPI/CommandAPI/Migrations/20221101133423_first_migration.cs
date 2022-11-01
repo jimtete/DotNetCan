fe4a20c6d80fa600ceabe3aa7ff3e1fc86dc079b
jimtete <tetepoulidis@ieee.org>
2022-11-01 16:02:52 +0200

    creating first endpoints

---
+++ CommandAPI/CommandAPI/Migrations/20221101133423_first_migration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CommandAPI.Migrations
+{
+    public partial class first_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CommandId = table.Column<string>(type: "text", nullable: false),
+                    HowTo = table.Column<string>(type: "text", nullable: false),
+                    Platform = table.Column<string>(type: "text", nullable: false),
+                    CommandLine = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+        }
+    }
+}
--- CommandAPI/CommandAPI/Program.cs
+++ CommandAPI/CommandAPI/Program.cs
@@ -1,7 +1,14 @@
+using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
@@ -12,5 +19,47 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//GET SINGLE
+app.MapGet("api/v1/commands/{commandId}", async (AppDbContext context, string commandId) =>
+{
+    var command = await context.Commands.FirstOrDefaultAsync(c => c.CommandId == commandId);
+
+    if (command == null)
+    {
+        return Results.NotFound();
+    }
+    
+    return Results.Ok(command);
+});
+
+//GET MULTIPLE
+app.MapGet("api/v1/commands", async (AppDbContext context) =>
+{
+    var commands = await context.Commands.ToListAsync();
+
+    return Results.Ok(commands);
+});
+/*
+//CREATE
+app.MapGet("api/v1/commands", async (AppDbContext context) =>
+{
+
+});
+
+//UPDATE
+app.MapGet("api/v1/commands", async (AppDbContext context) =>
+{
+
+});
+
+//DELETE
+app.MapGet("api/v1/commands", async (AppDbContext context) =>
+{
+
+});*/
+
+
+
 
 app.Run();
