ff6c55c423d4df5efe0ff09bbfc66604ec175b28
jimtete <tetepoulidis@ieee.org>
2022-11-08 13:35:21 +0200

    Playing with DTOs

---
+++ PersonAPI/Migrations/20221108113504_initial_migration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PersonAPI.Migrations
+{
+    public partial class initial_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FullName = table.Column<string>(type: "text", nullable: false),
+                    Telephone = table.Column<string>(type: "text", nullable: false),
+                    DoB = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
--- Playground/Playground/Program.cs
+++ Playground/Playground/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿int min;
+int[] arr = new[] { -2, -9, 2, -3, 1, 0 };
+
+min = GetMinimumElement(arr, 6);
+
+Console.WriteLine(min);
+
+
+static int GetMinimumElement(int[] arr, int length)
+{
+    if (length == 1)
+    {
+        return arr[0];
+    }
+
+    if (arr[length - 1] < GetMinimumElement(arr, length - 1))
+    {
+        return arr[length - 1];
+    }
+    return GetMinimumElement(arr, length - 1);
+}
